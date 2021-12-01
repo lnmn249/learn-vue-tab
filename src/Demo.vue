48eaef23a468a110d1526d16fd03ba1c9a8e8b3e
lnmn249 <dale@dghartung.com>
2021-11-30 20:05:05 -0600

    Update Demo.vue

--- src/Demo.vue
+++ src/Demo.vue
@@ -1,6 +1,5 @@
 <template lang="html">
-  <div class='wrapper'>
-    <button class='change__style' @click='changeStyle()'>Change Style</button>
+  <div>
     <tabs :mode="mode">
       <tab title="Tab 1">Hello From Tab 1</tab>
       <tab title="Tab 2">Hello From Tab 2</tab>
@@ -11,31 +10,21 @@
 </template>
 
 <script>
-import Tab from './Tab.vue'
-import Tabs from './Tabs.vue'
+  import Tab from './Tab.vue'
+  import Tabs from './Tabs.vue'
 
-export default {
-  components: {
-    Tab,
-    Tabs
-  },
-  data () {
-    return {
-      mode: 'dark'
-    }
-  },
-  methods: {
-    changeStyle () {
-      if (this.mode === 'dark') {
-        this.mode = 'light'
-      } else {
-        this.mode = 'dark'
+  export default {
+    components: {
+      Tab,
+      Tabs
+    },
+    data () {
+      return {
+        mode: 'light'
       }
     }
   }
-}
 </script>
-
 <style lang="css">
   * {
     margin: 0;
@@ -48,12 +37,5 @@ export default {
     background-color: #f8f8f8;
     margin: 0;
     padding: 20px;
-  }
-
-  .change__style {
-    background-color: #eee;
-    font-size: 1em;
-    margin-bottom: 10px;
-    padding: 5px;
   }
 </style>
